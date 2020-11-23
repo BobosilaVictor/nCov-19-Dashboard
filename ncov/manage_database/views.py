ab0746eb2f5ce95996a0c0f0287f9d81dd764675
NicolaeBarbulescu <barbulescu_nicu2010@yahoo.com>
2020-11-23 18:18:57 +0200

    Upload county coordinates from csv

--- ncov/manage_database/views.py
+++ ncov/manage_database/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-import pandas as pd
 from django.contrib import messages
 from django.contrib.auth.decorators import permission_required
 from .models import County, DailyReport
-
+import csv
+import io
+import pandas as pd
 
 # Create your views here.
 
@@ -19,17 +20,27 @@ def county_upload(request):
     if request.method == 'GET':
         return render(request, template, prompt)
 
-    csv_file = request.FILES('file')
+    csv_file = request.FILES['file']
 
     if not csv_file.name.endswith('.csv'):
         messages.error(request, 'Not a csv file!')
+    # data_set = csv_file.read().decode('UTF-8')
+    # io_string = io.StringIO(data_set)
+    # next(io_string)
+    # for column in csv.reader(io_string, delimiter=',', quotechar="|"):
+    #     _, created = County.objects.update_or_create(
+    #         name=column[0],
+    #         latitude=column[1],
+    #         longitude=column[2],
+    #     )
+    file = pd.read_csv(csv_file.name)
 
-    file = pd.read_csv(csv_file)
     for row in file.values:
-        obj = County.objects.update_or_create(
+        print(row[0],row[1],row[2])
+        County.objects.update_or_create(
             name=row[0],
             latitude=row[1],
             longitude=row[2],
-        )
+                )
     context = {}
     return render(request, template, context)
